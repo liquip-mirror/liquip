25c33bc61a7dbf4344d4051298c3765928c42441
zarlo <5899@zarlo.dev>
2023-03-26 19:52:29 +1100

    .

--- Test/Kernel.cs
+++ Test/Kernel.cs
@@ -23,43 +23,43 @@ namespace Test
         
         protected override void Run()
         {
-            Console.Write("RDseed.IsSupported: ");
-            Console.WriteLine(Cosmos.Zarlo.RDseed.IsSupported());
-            var rdSeed64 = Cosmos.Zarlo.RDseed.GetRDSeed64();
-            var rdSeed32 = Cosmos.Zarlo.RDseed.GetRDSeed32();
-            Console.Write("rdSeed64:");
-            Console.WriteLine((ulong)rdSeed64);
-            Console.Write("rdSeed32:");
-            Console.WriteLine((uint)rdSeed32);
-            Console.WriteLine();
-            var rdSeed64_0 = Cosmos.Zarlo.RDseed.GetRDSeed64();
-            var rdSeed32_0 = Cosmos.Zarlo.RDseed.GetRDSeed32();
-            Console.Write("rdSeed64:");
-            Console.WriteLine((ulong)rdSeed64_0);
-            Console.Write("rdSeed32:");
-            Console.WriteLine((uint)rdSeed32_0);
-
-            Console.WriteLine();
-            Console.Write("rdSeed64:");
-            Console.WriteLine(rdSeed64 != rdSeed64_0);
-            Console.Write("rdSeed32:");
-            Console.WriteLine(rdSeed32 != rdSeed32_0);
-            // Console.WriteLine(rdSeed16 != rdSeed16_0);
-
-            var powerManagementInformation = Cosmos.Zarlo.CPUID.PowerManagementInformation;
-            
-            Console.WriteLine("PowerManagementInformation");
-            Console.WriteLine(powerManagementInformation.DebugString());
-
-            var cacheConfiguration = Cosmos.Zarlo.CPUID.CacheConfiguration;
-            
-            Console.WriteLine("CacheConfiguration");
-            Console.WriteLine(cacheConfiguration.DebugString());
-            
-            var processorFrequencyInformation = Cosmos.Zarlo.CPUID.ProcessorFrequencyInformation;
-            
-            Console.WriteLine("ProcessorFrequencyInformation");
-            Console.WriteLine(processorFrequencyInformation.DebugString());
+            // Console.Write("RDseed.IsSupported: ");
+            // Console.WriteLine(Cosmos.Zarlo.RDseed.IsSupported());
+            // var rdSeed64 = Cosmos.Zarlo.RDseed.GetRDSeed64();
+            // var rdSeed32 = Cosmos.Zarlo.RDseed.GetRDSeed32();
+            // Console.Write("rdSeed64:");
+            // Console.WriteLine((ulong)rdSeed64);
+            // Console.Write("rdSeed32:");
+            // Console.WriteLine((uint)rdSeed32);
+            // Console.WriteLine();
+            // var rdSeed64_0 = Cosmos.Zarlo.RDseed.GetRDSeed64();
+            // var rdSeed32_0 = Cosmos.Zarlo.RDseed.GetRDSeed32();
+            // Console.Write("rdSeed64:");
+            // Console.WriteLine((ulong)rdSeed64_0);
+            // Console.Write("rdSeed32:");
+            // Console.WriteLine((uint)rdSeed32_0);
+            //
+            // Console.WriteLine();
+            // Console.Write("rdSeed64:");
+            // Console.WriteLine(rdSeed64 != rdSeed64_0);
+            // Console.Write("rdSeed32:");
+            // Console.WriteLine(rdSeed32 != rdSeed32_0);
+            // // Console.WriteLine(rdSeed16 != rdSeed16_0);
+            //
+            // var powerManagementInformation = Cosmos.Zarlo.CPUID.PowerManagementInformation;
+            //
+            // Console.WriteLine("PowerManagementInformation");
+            // Console.WriteLine(powerManagementInformation.DebugString());
+            //
+            // var cacheConfiguration = Cosmos.Zarlo.CPUID.CacheConfiguration;
+            //
+            // Console.WriteLine("CacheConfiguration");
+            // Console.WriteLine(cacheConfiguration.DebugString());
+            //
+            // var processorFrequencyInformation = Cosmos.Zarlo.CPUID.ProcessorFrequencyInformation;
+            //
+            // Console.WriteLine("ProcessorFrequencyInformation");
+            // Console.WriteLine(processorFrequencyInformation.DebugString());
 
             new Uri("http://google.com");
             
