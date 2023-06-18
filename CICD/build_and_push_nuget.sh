18351a1bc0aa9909871d479378546a9927cff934
zarlo <5899@zarlo.dev>
2023-06-18 12:10:19 +1000

    edit cicd

--- CICD/build_and_push_nuget.sh
+++ CICD/build_and_push_nuget.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -e 
 cd $1
 
 dotnet nuget add source "${CI_API_V4_URL}/projects/42768959/packages/nuget/index.json" --name gitlab --username gitlab-ci-token --password $CI_JOB_TOKEN --store-password-in-clear-text
