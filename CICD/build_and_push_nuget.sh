a5170617689ff0c17ea919348ec967805d72f502
zarlo <5899@zarlo.dev>
2023-07-18 00:18:42 +1000

    update cicd

--- CICD/build_and_push_nuget.sh
+++ CICD/build_and_push_nuget.sh
@@ -5,9 +5,9 @@ cd $1
 
 dotnet nuget add source "${CI_API_V4_URL}/projects/42768959/packages/nuget/index.json" --name gitlab --username gitlab-ci-token --password $CI_JOB_TOKEN --store-password-in-clear-text
 
-dotnet build /p:Version="0.1.1-alpha.${CI_PIPELINE_IID:-local}"
-dotnet pack /p:Version="0.1.1-alpha.${CI_PIPELINE_IID:-local}"
-dotnet pack /p:Version="0.1.1-alpha.${CI_PIPELINE_IID:-local}" /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg
+dotnet build /p:Version="0.1.${CI_PIPELINE_IID:-local}-alpha"
+dotnet pack /p:Version="0.1.${CI_PIPELINE_IID:-local}-alpha"
+dotnet pack /p:Version="0.1.${CI_PIPELINE_IID:-local}-alpha" /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg
 dotnet nuget push "bin/Debug/*.nupkg" --source gitlab
 dotnet nuget push "bin/Debug/*.snupkg" --source gitlab
 
